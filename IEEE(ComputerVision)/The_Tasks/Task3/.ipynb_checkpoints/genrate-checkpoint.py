83bfc50b1dca5c9b57bb5c8d2747cf050f11f5a7
MoHaMeD <mo7amed3twan@gmail.com>
2023-11-12 23:53:39 +0200

    1

---
+++ IEEE(ComputerVision)/The_Tasks/Task3/.ipynb_checkpoints/genrate-checkpoint.py
@@ -0,0 +1,18 @@
+import os
+import cv2
+
+os.makedirs("folders")
+os.chdir(r'E:\Ai\CV\session 4\task\folders')
+for i in range(5):
+    os.makedirs(f"folder{i+1}")
+
+x = 1
+for folder_name in os.listdir(r'E:\Ai\CV\session 4\task\folders') :
+    imgs = cv2.imread(fr'E:\Ai\CV\session 4\task\images\{x}.jpg')
+    x = x + 1
+    path = fr"E:\Ai\CV\session 4\task\folders\{folder_name}"
+    os.chdir(path)
+    for i in range(50):
+        cv2.imwrite(f"{i+1}.jpg", imgs)
+
+print(" complete.")
